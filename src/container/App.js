6b2570031188d8bf379868aaed4724f0d47b1d4c
Nikesh Dhakal <a46351@alunos.ipb.pt>
2023-01-04 04:50:50 +0000

    Created function to map the state and dipatch action to change the state and at last destructured the props and got setSearchFiled and onSearchChange function

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import CardList from "../components/CardList.js";
-// import { robots } from "./robot.js";
+import { connect } from "react-redux";
 import SearchBox from "../components/SearchBox.js";
 import "./App.css";
 import "../fonts/SEGA.TTF";
 import Scroll from "../components/Scroll.js";
 import ErrorBoundry from "../components/ErrorBoundry.js";
+import { setSearchField } from "../action.js";
 
-export default function App() {
+function App(props) {
   const [robots, setRobots] = useState([]);
-  const [searchField, setSearchField] = useState("");
+  const { searchField, onSearchChange } = props;
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -17,9 +18,9 @@ export default function App() {
       .then((users) => setRobots(users));
   }, []);
 
-  const onSearchChange = (event) => {
-    setSearchField(event.target.value);
-  };
+  // const onSearchChange = (event) => {
+  //   setSearchField(event.target.value);
+  // };
 
   const filterRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
@@ -41,3 +42,17 @@ export default function App() {
     </>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
 import App from "./container/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
+const store = createStore(searchRobots);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
